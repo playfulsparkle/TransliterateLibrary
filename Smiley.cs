e4cb93333383a465cd023acccfffa3735d97d707
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-04-04 20:12:05 +0200

    base mapping added

---
+++ Smiley.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayfulSparkle
+{
+    internal class Smiley
+    {
+        public static Dictionary<string, string> chars = new Dictionary<string, string>()
+        {
+            {"U+1F600", "grinning face" },
+            {"U+1F603", "grinning face with big eyes" },
+            {"U+1F604", "grinning face with smiling eyes" },
+            {"U+1F601", "beaming face with smiling eyes" },
+            {"U+1F606", "grinning squinting face" },
+            {"U+1F605", "grinning face with sweat" },
+            {"U+1F923", "rolling on the floor laughing" },
+            {"U+1F602", "face with tears of joy" },
+            {"U+1F642", "slightly smiling face" },
+            {"U+1F643", "upside-down face" },
+            {"U+1FAE0", "melting face" },
+            {"U+1F609", "winking face" },
+            {"U+1F60A", "smiling face with smiling eyes" },
+            {"U+1F607", "smiling face with halo" },
+            {"U+1F970", "smiling face with hearts" },
+            {"U+1F60D", "smiling face with heart-eyes" },
+            {"U+1F929", "star-struck" },
+            {"U+1F618", "face blowing a kiss" },
+            {"U+1F617", "kissing face" },
+            {"U+263A", "smiling face" },
+            {"U+1F61A", "kissing face with closed eyes" },
+            {"U+1F619", "kissing face with smiling eyes" },
+            {"U+1F972", "smiling face with tear" },
+            {"U+1F60B", "face savoring food" },
+            {"U+1F61B", "face with tongue" },
+            {"U+1F61C", "winking face with tongue" },
+            {"U+1F92A", "zany face" },
+            {"U+1F61D", "squinting face with tongue" },
+            {"U+1F911", "money-mouth face" },
+            {"U+1F917", "smiling face with open hands" },
+            {"U+1F92D", "face with hand over mouth" },
+            {"U+1FAE2", "face with open eyes and hand over mouth" },
+            {"U+1FAE3", "face with peeking eye" },
+            {"U+1F92B", "shushing face" },
+            {"U+1F914", "thinking face" },
+            {"U+1FAE1", "saluting face" }
+        };
+    }
+}
--- Transliterate.cs
+++ Transliterate.cs
@@ -1,10 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace PlayfulSparkle
 {
     public class Transliterate
     {
+        public enum Normalization
+        {
+            Decompose, // NFD
+            Compose, // NFC
+            CompatibilityCompose, // NFKC
+            CompatibilityDecompose // NFKD
+        }
+
+        public static string Decompose(string str, Normalization normalization)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return str;
+            }
+
+            NormalizationForm normalizationForm;
+
+            switch (normalization)
+            {
+                case Normalization.Decompose:
+                    normalizationForm = NormalizationForm.FormD;
+                    break;
+                case Normalization.Compose:
+                    normalizationForm = NormalizationForm.FormC;
+                    break;
+                case Normalization.CompatibilityCompose:
+                    normalizationForm = NormalizationForm.FormKC;
+                    break;
+                case Normalization.CompatibilityDecompose:
+                    normalizationForm = NormalizationForm.FormKD;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(normalization), normalization, null);
+            }
+
+            string normalized = str.Normalize(normalizationForm);
+
+            // Remove combining marks
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in normalized)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    // Apply character mapping if available
+                    string charStr = c.ToString();
+
+                    if (Mappings.chars.TryGetValue($"U+{(int)c:X4}", out string unicodeReplacement))
+                    {
+                        result.Append(unicodeReplacement);
+                    }
+                    if (Smiley.chars.TryGetValue($"U+{(int)c:X4}", out string smileyReplacement))
+                    {
+                        result.Append(smileyReplacement);
+                    }
+                    else
+                    {
+                        result.Append(charStr);
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
     }
 }
